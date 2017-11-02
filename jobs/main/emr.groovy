337f91c15fca4c293571c40214982ccfdd51f66a
Baber Khalil <Baber.s.Khalil@googlemail.com>
2017-11-02 09:36:50 +0000

    Added EMR project.

---
+++ jobs/main/emr.groovy
@@ -0,0 +1,48 @@
+package main
+
+def gitUrl = 'https://github.com/baber/terraform-emr.git'
+
+job('terraform') {
+    scm {
+        git(gitUrl)
+    }
+    triggers {
+        scm('*/5 * * * *')
+    }
+    steps {
+        shell('./run.sh')
+    }
+    publishers {
+        s3BucketPublisher {
+            profileName('default')
+            entries {
+                entry {
+                    bucket('com.ee.bdec.coderepo')
+                    sourceFile('target/*.tgz')
+                    excludedFile('')
+                    storageClass('STANDARD')
+                    selectedRegion('eu-west-2')
+                    noUploadOnFailure(true)
+                    uploadFromSlave(true)
+                    managedArtifacts(false)
+                    useServerSideEncryption(false)
+                    flatten(false)
+                    gzipFiles(false)
+                    keepForever(false)
+                    showDirectlyInBrowser(false)
+                    userMetadata {}
+                }
+            }
+            userMetadata {
+                metadataPair {
+                    key('key')
+                    value('value')
+                }
+            }
+            dontWaitForConcurrentBuildCompletion(false)
+            consoleLogLevel('INFO')
+            pluginFailureResultConstraint('FAILURE')
+        }
+    }
+
+}
